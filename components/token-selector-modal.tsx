1ec1b104e5c2d638c09bd97eee6f0d3d36749a24
svntax <5898368+svntax@users.noreply.github.com>
2025-09-06 08:43:56 -0400

    Remove unused import and add lint ignore lines.

--- components/token-selector-modal.tsx
+++ components/token-selector-modal.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useMemo } from "react";
+import { useEffect, useState } from "react";
 import { XIcon } from "./x-icon";
 import Image from "next/image";
 
--- lib/useDebounce.ts
+++ lib/useDebounce.ts
@@ -1,5 +1,7 @@
 import { useState, useEffect } from "react";
+// eslint-disable-next-line  @typescript-eslint/no-explicit-any
 export const useDebounce = (value: any, delay: number) => {
+    // eslint-disable-next-line  @typescript-eslint/no-explicit-any
     const [debounced, setDebounced] = useState<any>(value);
     useEffect(() => {
         const handler = setTimeout(() => setDebounced(value), delay);
